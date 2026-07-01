2d095750da53336b2d11dbc05ffff950965f88bf
Lutz Schönemann <lutz.schoenemann+github@gmail.com>
2026-07-01 23:21:29 +0200

    Adds application routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { BierEvent } from './bier-event/bier-event';
+import { BiergitComponent } from './biergit-component/biergit-component';
+import { AcceptedOfferReceiver } from './bier-event/accepted-offer-receiver/accepted-offer-receiver';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: BierEvent,
+    },
+    {
+        path: 'accepted/:answer',
+        component: AcceptedOfferReceiver,
+    },
+    {
+        path: 'biergit',
+        component: BiergitComponent,
+    }
+];
